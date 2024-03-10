0eb0ec9e918c4d99b236d66741f6ee992dce4dac
AdyaTech <93500258+AdyaTech@users.noreply.github.com>
2024-03-10 14:29:57 +0530

    Create About Seven-Segment Display.py

---
+++ Day 42/About Seven-Segment Display.py
@@ -0,0 +1,3 @@
+# Introduction
+A seven-segment display is a type of LCD component used to display numbers in pocket calculators, microwave ovens, and other small electronic devices. Through different combinations of seven line-shaped segments in an LCD, a seven-segment display can represent the digits 0 through 9.  
+
